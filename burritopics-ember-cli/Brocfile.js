689f88270180f6d5bc50c5144f72f26ea544dfcb
Kris Komar <kkomar@scentsy.com>
2015-03-27 08:45:17 -0600

    upgrade to ember cli 0.2.1

--- burritopics-ember-cli/Brocfile.js
+++ burritopics-ember-cli/Brocfile.js
@@ -2,11 +2,7 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp({
-    'ember-cli-jquery-ui': {
-        'theme': 'hot-sneaks'
-    }
-});
+var app = new EmberApp();
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
@@ -20,6 +16,5 @@ var app = new EmberApp({
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
-
 
 module.exports = app.toTree();

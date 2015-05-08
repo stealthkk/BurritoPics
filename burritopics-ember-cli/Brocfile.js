3171358cf603f0f49fecc095be83e18566363e2f
Kris Komar <kkomar@scentsy.com>
2015-05-08 13:36:27 -0600

    now works on live!

--- burritopics-ember-cli/Brocfile.js
+++ burritopics-ember-cli/Brocfile.js
@@ -2,7 +2,12 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  storeConfigInMeta: false,
+  fingerprint: {
+    enabled: false
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.

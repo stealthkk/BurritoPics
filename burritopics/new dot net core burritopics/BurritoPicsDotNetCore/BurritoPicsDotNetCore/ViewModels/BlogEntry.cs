85a95c49a95dc59f7141c8241c1faf8091bbc4be
Kristopher Komar <kris@Kristophers-MacBook-Pro.local>
2017-04-05 09:40:15 -0600

    new solution

---
+++ burritopics/new dot net core burritopics/BurritoPicsDotNetCore/BurritoPicsDotNetCore/ViewModels/BlogEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BurritoPicsDotNetCore.ViewModels
+{
+    public class BlogEntry
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublishDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
---
+++ burritopics/new dot net core burritopics/BurritoPicsDotNetCore/BurritoPicsDotNetCore/ViewModels/Pic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BurritoPicsDotNetCore.ViewModels
+{
+    public class Pic
+    {
+        public string FileName { get; set; }
+        public DateTime DateSubmitted { get; set; }
+        public DateTime DateApproved { get; set; }
+        public bool Approved { get; set; }
+
+    }
+}

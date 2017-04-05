b4a6e41e9783f965ea7cf9284cf100579d319b53
Kristopher Komar <kris@Kristophers-MacBook-Pro.local>
2017-04-05 08:54:00 -0600

    Moving old solution

---
+++ old versions/Old dot net core BurritoPics/src/BurritoPics/obj/Release/net462/win7-x86/PubTmp/Out/wwwroot/js/slowimage.js
@@ -0,0 +1,10 @@
+export default function getRandRange (low, high) {
+        var adjustedHigh = (parseFloat(high) - parseFloat(low)) + 1;
+        var numRand = Math.floor(Math.random()*adjustedHigh) + parseFloat(low);
+
+        if (parseFloat(low) <= parseFloat(high) && (low !== '') && (high !== '')) {
+            return numRand;
+        } else {
+            return 0;
+        }
+    }
